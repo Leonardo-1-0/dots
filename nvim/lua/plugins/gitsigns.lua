6f64c70f6cc50539d226683bbbb029c8b363effe
“LeoPedri” <leonardopedri1.0@gmail.com>
2023-11-14 14:41:44 +0100

    fix wonky formatting

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,11 +1,12 @@
 return {
-	{ "lewis6991/gitsigns.nvim",
+    "lewis6991/gitsigns.nvim",
 
-	opts = { signs = { add = { text = "+" },
-		change = { text = "~" },
-		delete = { text = "-" },
-		untracked = { text = "*" },
-			}
-		}
-	}
+    opts = {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "-" },
+            untracked = { text = "*" },
+        },
+    },
 }
