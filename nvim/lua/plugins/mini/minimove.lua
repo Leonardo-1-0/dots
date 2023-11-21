52b3cfec7fa56d4da4cf28b4b0aedb5876b633da
Leonardo Pedri <leonardopedri1.0@gmail.com>
2023-11-21 14:15:07 +0100

    fix fault minimove config

--- nvim/lua/plugins/mini/minimove.lua
+++ nvim/lua/plugins/mini/minimove.lua
@@ -1,20 +1,5 @@
 return {
     "echasnovski/mini.move",
 
-    keys = {
-        -- Module mappings. Use `''` (empty string) to disable one.
-        mappings = {
-            -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-            left = '<M-h>',
-            right = '<M-l>',
-            down = '<M-j>',
-            up = '<M-k>',
-
-            -- Move current line in Normal mode
-            line_left = '',
-            line_right = '',
-            line_down = '',
-            line_up = '',
-        },
-    }
+    opts = {},
 }
