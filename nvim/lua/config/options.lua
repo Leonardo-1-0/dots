2e1e3e9fe94684268b1610ebdc84561b6db50264
“LeoPedri” <leonardopedri1.0@gmail.com>
2023-11-25 14:11:41 +0100

    adjust folding

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -19,23 +19,28 @@ vim.o.softtabstop = 4 -- Number of spaces to insert for TAB in insert mode
 vim.o.expandtab = true -- Use spaces instead of tabs
 vim.o.smartindent = true
 
-vim.opt.guicursor = "" -- Make cursor fat in insert-mode
+vim.o.guicursor = "" -- Make cursor fat in insert-mode
 
 vim.g.autoformat = true
-vim.opt.cursorline = true -- Enable highlighting of the current line
-vim.opt.list = true -- Show some invisible characters (tabs...
-vim.opt.mouse = "a" -- Enable mouse mode
-vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.opt.termguicolors = true -- True color support so notify doesn't bitch
-vim.opt.colorcolumn = "120"
-vim.opt.scrolloff = 8
-vim.opt.swapfile = false
-vim.opt.splitright = true
+vim.o.cursorline = true -- Enable highlighting of the current line
+vim.o.list = true -- Show some invisible characters (tabs...
+vim.o.mouse = "a" -- Enable mouse mode
+vim.o.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.o.termguicolors = true -- True color support so notify doesn't bitch
+vim.o.colorcolumn = "120"
+vim.o.scrolloff = 8
+vim.o.swapfile = false
+vim.o.splitright = true
 
 -- folding
-vim.opt.foldmethod = "indent" -- set folding groups to be automatically detected by indentation (as opposed to manually)
-vim.cmd("set foldlevelstart=99") -- set all folds open when entering a buffer. Set to 0 to have them all closed when opening a buffer.
-vim.keymap.set({ "n", "v" }, "<leader>zc", "zC", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>zo", "zO", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>zm", "zM", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>zn", "zn", { noremap = true, silent = true })
+vim.o.foldmethod = "indent" -- set folding groups to be automatically detected by indentation (as opposed to manually)
+vim.cmd("set foldlevelstart=5") -- set all folds open when entering a buffer. Set to 0 to have them all closed when opening a buffer.
+local function map(l, r)
+    vim.keymap.set({ "n", "v" }, l, r, { noremap = true, silent = true })
+end
+map("<leader>zc", "zc")
+map("<leader>zo", "zo")
+map("<leader>zC", "zC")
+map("<leader>zO", "zO")
+map("<leader>zm", "zM")
+map("<leader>zn", "zn")
