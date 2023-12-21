4212a128233ddec49c807d71ab521819e55896db
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2023-12-21 16:12:30 +0100

    first stab at waybar styling

--- hypr/sleep.sh
+++ hypr/sleep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-swayidle -w timeout 300 'swaylock -f -c 000000' \
+swayidle -w timeout 300 'swaylock -f -i /home/leo/Pictures/synthwave_2.png --indicator-idle-visible --indicator-radius 120 --indicator-thickness 20 --line-color FFFFFF --line-wrong-color FF0000 --ring-color FFFFFF' \
     timeout 600 'systemctl suspend' \
-    before-sleep 'swaylock -f -c 000000' &
+    before-sleep 'swaylock -f -i /home/leo/Pictures/synthwave_2.png --indicator-idle-visible --indicator-radius 120 --indicator-thickness 20 --line-color FFFFFF --line-wrong-color FF0000 --ring-color FFFFFF' &
