69711556337c698111feeb1750aa8a8a46937e48
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-01-04 07:51:13 +0100

    background script also updates waybar now

--- hypr/background.sh
+++ hypr/background.sh
@@ -1,8 +1,13 @@
 #WALLPAPER="/home/leo/Pictures/synthwave.jpg"
 WALLPAPER="/home/leo/Pictures/halo.jpg"
+#WALLPAPER="/home/leo/Pictures/netrun_vol3.png"
+
+WAYBAR_CONFIG="/home/leo/.config/waybar"
 
 wal -i ${WALLPAPER}
 swww kill
 swww init
 swww img ${WALLPAPER}
+pkill -f "waybar"
+waybar -c ${WAYBAR_CONFIG}/config.json -s ${WAYBAR_CONFIG}/style.css &
 /home/leo/.config/swaylock/pyWalConfig.py
