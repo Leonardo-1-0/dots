bc791926951b1fcda7b3782fb4e2cc8ec5541737
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-01-17 20:57:28 +0100

    start night light at night time

--- hypr/background.sh
+++ hypr/background.sh
@@ -1,8 +1,17 @@
 #WALLPAPER="/home/leo/Pictures/synthwave.jpg"
-WALLPAPER="/home/leo/Pictures/halo.jpg"
-#WALLPAPER="/home/leo/Pictures/netrun_vol3.png"
+#WALLPAPER="/home/leo/Pictures/halo.jpg"
+WALLPAPER="/home/leo/Pictures/Sunset-by-the-Pier.jpg"
 
 WAYBAR_CONFIG="/home/leo/.config/waybar"
+
+activate_night_light() {
+	currenttime=$(date +%H:%M)
+	if [[ "$currenttime" > "20:00" ]] || [[ "$currenttime" < "07:00" ]]; then
+		busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Brightness d 1.0 &&
+			busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay \
+				Temperature q 2750
+	fi
+}
 
 wal -i ${WALLPAPER}
 swww kill
@@ -11,3 +20,4 @@ swww img ${WALLPAPER}
 pkill -f "waybar"
 waybar -c ${WAYBAR_CONFIG}/config.json -s ${WAYBAR_CONFIG}/style.css &
 /home/leo/.config/swaylock/pyWalConfig.py
+activate_night_light
