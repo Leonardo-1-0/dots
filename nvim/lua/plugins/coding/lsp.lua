5a0d0a3f1eab1238871c58d6e528986e0d07add5
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-01-18 15:56:25 +0100

    move lsp diagnostics to floating window
    otherwise only like half are readable before being cut off by
    the length of the screen

--- nvim/lua/plugins/coding/lsp.lua
+++ nvim/lua/plugins/coding/lsp.lua
@@ -76,24 +76,18 @@ return {
         end
     end,
 
+    vim.diagnostic.config({ virtual_text = false, float = { source = 'always', show_header = true } }),
     keys = {
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts),
+        vim.keymap.set({ "n" }, "<leader>d", ":lua vim.diagnostic.open_float(nil, { focus = false })<CR>",
+            { silent = true }),
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts),
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts),
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts),
         vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts),
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts),
-        -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts),
-        -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts),
-        -- vim.keymap.set("n", "<space>wl", function()
-        --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, opts),
-        vim.keymap.set("n", "<space>d", vim.lsp.buf.type_definition, opts),
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts),
-        vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts),
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts),
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts),
-        vim.keymap.set("n", "<space>f", function()
-            vim.lsp.buf.format({ async = true })
-        end, opts),
     },
 
     --[[     enabled = false ]]
